7f58e0f350f4cb1590ff3db56106a903ae069902
Kalle <kalle@kandru.de>
2025-03-22 18:03:16 +0100

    fix: ModuleAuthor corrected (copied from other plugin)

--- src/PanoramaVoteManager/PanoramaVoteManager.cs
+++ src/PanoramaVoteManager/PanoramaVoteManager.cs
@@ -5,7 +5,7 @@ namespace PanoramaVoteManager
     public partial class PanoramaVoteManager : BasePlugin
     {
         public override string ModuleName => "CS2 PanoramaVoteManager";
-        public override string ModuleAuthor => "Originally by Franc1sco Franug / rewritten by Jon-Mailes Graeffe <mail@jonni.it> and Kalle <kalle@kandru.de>";
+        public override string ModuleAuthor => "Kalle <kalle@kandru.de>";
 
         public override void Load(bool hotReload)
         {
