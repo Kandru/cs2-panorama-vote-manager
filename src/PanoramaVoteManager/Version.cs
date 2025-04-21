70ddf92c21c88d737d70863c284b3dc7dbcfcca0
Kalle <kalle@kandru.de>
2025-04-21 22:19:56 +0200

    bump version to 0.1.10

--- src/PanoramaVoteManager/Version.cs
+++ src/PanoramaVoteManager/Version.cs
@@ -2,5 +2,5 @@ namespace PanoramaVoteManager;
 
 public partial class PanoramaVoteManager
 {
-    public override string ModuleVersion => "0.1.9";
+    public override string ModuleVersion => "0.1.10";
 }
