bfb371c0293f7ce46bcce5c4b745625d24a25ac6
derkalle4 <kalle@kandru.de>
2026-04-23 15:20:49 +0200

    bump version to 26.04.1

--- src/PanoramaVoteManager/Version.cs
+++ src/PanoramaVoteManager/Version.cs
@@ -2,5 +2,5 @@ namespace PanoramaVoteManager;
 
 public partial class PanoramaVoteManager
 {
-    public override string ModuleVersion => "0.1.13";
+    public override string ModuleVersion => "26.04.1";
 }
